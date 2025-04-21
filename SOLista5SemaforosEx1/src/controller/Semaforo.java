8a99d93a6d9c9a3176165e0949e0353928442554
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2025-04-20 23:21:09 -0300

    criando as funções de cálculo e transação

---
+++ SOLista5SemaforosEx1/src/controller/Semaforo.java
@@ -0,0 +1,58 @@
+package controller;
+
+public class Semaforo extends Thread {
+
+	private static int id = 1;
+
+	public Semaforo() {
+	}
+
+	@Override
+	public void run() {
+
+		if (id % 3 == 1) {
+
+			calc(200, 1000);
+
+		} else if (id % 3 == 1) {
+
+		} else if (id % 3 == 1) {
+
+		}
+
+	}
+
+	// Tempo de cálculo da thread
+	private void calc(int min, int max) {
+
+		System.out.println("A thread " + id + " está realizando um cálculo");
+
+		int time = (int) ((Math.random() * (max - min + 1)) + min);
+
+		try {
+
+			sleep(time);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		System.out.println("A thread " + id + " finalizou um cálculo");
+
+	}
+
+	private void transacao(int add) {
+
+		int time = 1000 + add;
+
+		try {
+			sleep(time);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
